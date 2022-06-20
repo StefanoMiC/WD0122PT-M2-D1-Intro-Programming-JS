6087ebac18e0f844adb9ced5b74af74eaed55664
Stefano Miceli <mic@nucleode.com>
2022-06-20 22:12:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// first variable
+let hello = "HI!";
+// console.log("first log " + hello);
+
+/* second variable */
+let greet = hello + "?";
+// console.log("second log", greet);
+
+/* Pasta alla bolognese */
+
+let water = 3;
+let pasta = 200;
+let salt = 30;
+
+let pot = water + salt;
+console.log(pot);
+
+// pot = pot + pasta;
+pot += pasta;
+console.log(pot);
+
+console.log("3" + 34);
+
+// STRING, NUMBER, BOOLEAN, UNDEFINED, NULL
+
+let sum;
+console.log(sum);
+sum = 12;
+console.log(sum);
+
+// BOOLEAN: TRUE / FALSE
+
+let total = 10 + 2;
+
+console.log("EQUALITY CHECK", sum === total); // triple equals for equality checks
+console.log(sum <= total);
